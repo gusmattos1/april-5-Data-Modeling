6a83c5e12022b2dadbaa2b09405e4017d9f55c2f
gusmattos1 <gusmattos@gmail.com>
2018-04-05 14:07:30 -0400

    Exercise 1 done

---
+++ exercise1.rb
@@ -0,0 +1,24 @@
+hash = { data: {
+              rooms: [
+              { id: 1, room_number: "201", capacity: 50},
+              { id: 2, room_number: "301", capacity: 200},
+              { id: 3, room_number: "1B", capacity: 100}
+                      ],
+              events: [
+              { id: 1, room_id: 2, start_time: 18, end_time: 20, attendees: 75 },
+              { id: 2, room_id: 1, start_time: 10, end_time: 18, attendees: 25 },
+              { id: 3, room_id: 2, start_time: 10, end_time: 18, attendees: 20 },
+              { id: 4, room_id: 3, start_time: 18, end_time: 21, attendees: 56 },
+                      ]
+                }
+        }
+
+room_201 = hash[:data][:rooms][0][:capacity]
+
+puts room_201
+
+hash[:data][:events].each do |array|
+  if array[:room_id] == 1 && array[:attendees] <= room_201
+    puts "OK"
+  end
+end
